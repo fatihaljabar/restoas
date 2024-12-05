e32f117a0e5a94aacbc12132e36c464fc4b1e9bb
Fatih Al Jabar <fatihaljabar@gmail.com>
2024-12-05 19:14:10 +0700

    Add dotenv and reconfigure webpack and jest

--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
-  KEY: 'TEST',
-  BASE_URL: 'https://restaurant-api.dicoding.dev',
-  BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/large/',
-  CACHE_NAME: new Date().toISOString(),
-  DATABASE_NAME: 'restaurant-catalogue-database',
-  DATABASE_VERSION: 1,
-  OBJECT_STORE_NAME: 'restaurant',
+    KEY: process.env.KEY,
+    BASE_URL: 'https://restaurant-api.dicoding.dev',
+    BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/large/',
+    CACHE_NAME: new Date().toISOString(),
+    DATABASE_NAME: process.env.DATABASE_NAME,
+    DATABASE_VERSION: process.env.DATABASE_VERSION,
+    OBJECT_STORE_NAME: process.env.OBJECT_STORE_NAME,
 };
 
 export default CONFIG;
