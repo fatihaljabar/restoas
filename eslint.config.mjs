1a799ce57f08a653ad0383513377d3d7d8c32804
Fatih Al Jabar <fatihaljabar@gmail.com>
2024-12-05 23:07:17 +0700

    Fixing Linter and Add Code Splitting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,15 +3,15 @@ import pluginJs from "@eslint/js";
 import daStyle from "eslint-config-dicodingacademy";
 
 export default [
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  daStyle,
+    { languageOptions: { globals: {...globals.browser, process: "readonly" } } }, // Add process as a global
+    pluginJs.configs.recommended,
+    daStyle,
 
-  {
-    rules: {
-      "space-infix-ops": ["error"],
-      "brace-style": ["error", "1tbs"],
-      "space-before-blocks": ["error", "always"],
+    {
+        rules: {
+            "space-infix-ops": ["error"],
+            "brace-style": ["error", "1tbs"],
+            "space-before-blocks": ["error", "always"],
+        },
     },
-  },
 ];
